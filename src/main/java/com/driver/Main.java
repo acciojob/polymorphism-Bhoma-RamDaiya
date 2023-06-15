1033a43b2de862af93bb935cf4079dbaf0f7d4ea
Bhoma-RamDaiya <bhoma05061998@gmail.com>
2023-06-15 11:59:20 +0530

    solution added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,20 @@ package com.driver;
 import java.sql.SQLOutput;
 
 public class Main {
+
+
+    public static class Product {
+        public int product(int x , int y){
+            return x*y;
+        }
+        public int product(int x , int y, int z){
+            return x*y*z;
+        }
+        public double product(double x , double y){
+            return x*y;
+        }
+    }
+
     public static void main(String[] args) {
         Product p = new Product();
        int result = p.product(2, 5);
